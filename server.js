eb5a9ecedfff96d1c26c0ffe07265d0acfe31aa7
Luis Grande <grandel.programador@gmail.com>
2023-03-15 18:13:49 -0300

    cambio de la funcion Randon para buscar por id

--- server.js
+++ server.js
@@ -11,13 +11,25 @@ server.on('error', err =>console.log(`el error es: ${err}`))
 
 const products = new Contenedor('products.txt')
 
-app.get('/productos', async(rej,res)=>{
+app.get('/productos', async(req,res)=>{
     const productos = await products.getAll()
     res.send(productos)
 })
 
-app.get('/productoRandom',async(rej,res)=>{
-    const productos = await products.getAll()
-    let numeroRandom = Math.floor(Math.random() * productos.length)
-    res.send(productos[numeroRandom])
+app.get('/productos/:id',async(req,res)=>{
+
+   const productos = await products.getAll()
+    // let numeroRandom = Math.floor(Math.random() * productos.length)
+    // res.send(productos[numeroRandom])
+    // const producto = await products.getProductById()
+
+    let id = req.params.id
+
+    console.log(id)
+
+    let buscado = await productos.find(prod =>JSON.stringify(prod.id) === id)
+    
+    console.log(buscado)
+    if(!buscado) return res.send({error:"producto no encontrado"})
+    res.send({buscado})
 })
